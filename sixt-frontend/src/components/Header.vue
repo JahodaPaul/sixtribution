0d3ccb63c51f09cdba837089c28646f8a9f266ab
Zuzana Urbanová <zu10urbanova@gmail.com>
2022-11-19 14:15:54 +0100

    add header

---
+++ sixt-frontend/src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div class="top sticky flex flex-row p-8">
+    <img alt="SIXT logo" class="logo" src="@/assets/SixtLogo.svg" width="100" />
+    <div class="bg-red-500 text-3xl font-bold underline">This is header.</div>
+  </div>
+</template>
