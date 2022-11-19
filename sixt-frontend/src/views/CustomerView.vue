21b1ff6d8a0ce53d27d069714916d80ef850f3ee
Zuzana Urbanová <zu10urbanova@gmail.com>
2022-11-19 19:17:14 +0100

    timer

--- sixt-frontend/src/views/CustomerView.vue
+++ sixt-frontend/src/views/CustomerView.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import FetchData from "../components/FetchData.vue";
-import {computed} from "vue";
+import {computed, ref} from "vue";
 
 const endDate = Date.UTC(2022, 11, 20, 10, 0)
-const timer = computed(() => {
-  return (Date.now() - endDate)
-})
+
+const timer = ref(endDate - Date.now());
+let interval = setInterval(() => {
+  if (timer.value === 0) {
+    clearInterval(interval)
+  } else {
+    timer.value -= 1000
+    console.log("seconds: " + (timer.value / 1000).)
+    console.log("minutes: " + timer.value / 1000)
+    console.log("hours: " + timer.value / 1000)
+    console.log("days: " + timer.value / 1000)
+    console.log(new Date(timer.value))
+  }
+}, 1000)
 </script>
 
 <template>
