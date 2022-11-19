e938f768189ac153a96f46b025586ab9cfc2f200
Pavel Jahoda <pjahoda6@gmail.com>
2022-11-19 21:18:28 +0100

    Fixed date

--- sixt-frontend/src/views/CustomerView.vue
+++ sixt-frontend/src/views/CustomerView.vue
@@ -3,21 +3,27 @@ import FetchData from "../components/FetchData.vue";
 import {computed, ref} from "vue";
 
 const endDate = Date.UTC(2022, 11, 20, 10, 0)
-const today = new Date().getTime();
+const today = Date.UTC(new Date().getFullYear(), new Date().getMonth()+1, new Date().getDate(), new Date().getHours()+1, new Date().getMinutes()+1)
 
 const timer = ref(endDate - today);
+const days = ref(0)
+const hours = ref(0)
+const minutes = ref(0)
+const seconds = ref(0)
 let interval = setInterval(() => {
   if (timer.value === 0) {
     clearInterval(interval)
   } else {
     timer.value -= 1000
-    const days = Math.floor(timer.value / 1000 / 60 / 60 / 24)
-    const hours = ((timer.value - days*(1000*60*60*24)) / 1000 / 60 / 60).toFixed(0)
+    days.value = Math.floor(timer.value / 1000 / 60 / 60 / 24)
+    hours.value = Math.floor((timer.value - days.value*(1000*60*60*24)) / 1000 / 60 / 60)
+    minutes.value = Math.floor((timer.value - days.value*(1000*60*60*24) - hours.value*(1000*60*60)) / 1000 / 60 )
+    seconds.value = Math.floor((timer.value - days.value*(1000*60*60*24) - hours.value*(1000*60*60) - minutes.value*(1000*60)) / 1000)
     console.log(days, hours, Date.now())
     console.log("seconds: " + Math.floor(timer.value / 1000))
     console.log("minutes: " + (timer.value / 1000 / 60).toFixed(0))
     console.log("hours: " + (timer.value / 1000 / 60 / 60).toFixed(0))
-    console.log("days: " + (timer.value / 1000 / 60 / 60 / 24).toFixed(0))
+    console.log("days: " + new Date().getDay())
   }
 }, 1000)
 </script>
@@ -31,7 +37,7 @@ let interval = setInterval(() => {
 
     </div>
     <div>
-      Rental ends in {{ timer }}
+      Rental ends in {{ days }} days {{ hours }} hours {{ minutes }} minutes {{ seconds }} seconds
     </div>
     fetch data: <FetchData />
   <div class="bg-black text-white p-2 rounded-lg fixed hover:cursor-pointer fixed z-90 bottom-10 right-8 ">Button</div>
