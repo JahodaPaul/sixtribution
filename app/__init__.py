5c1c21d79eede335ae86efa445e633555701acd9
Pavel Jahoda <pjahoda6@gmail.com>
2022-11-19 15:36:15 +0100

    added CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from flask import Flask
+from flask_cors import CORS
 from app import main
 from app.main.api import api
 from app.main.database import db, migration
@@ -8,6 +9,7 @@ from app.main.logging import LOGGING_CONFIG
 
 # Flask App Initialization
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(main.settings[os.environ.get('APPLICATION_ENV', 'default')])
 
 # Logs Initialization
