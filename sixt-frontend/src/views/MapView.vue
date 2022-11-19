9953d55d1f44e10e151e7ae04d766905abc25b4b
Zuzana Urbanová <zu10urbanova@gmail.com>
2022-11-19 17:02:50 +0100

    add changes

--- sixt-frontend/src/views/MapView.vue
+++ sixt-frontend/src/views/MapView.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts">
+<script setup>
+import { GoogleMap, Marker } from 'vue3-google-map';
 
+const center = { lat: 48.145, lng: 11.550 }
 </script>
 
 <template>
-
+  <div class="bg-red-500">
+  <GoogleMap
+      api-key="AIzaSyCDzbtVQ0VGI-EaoCJat7kdT_vLAeSCcD4"
+      style="width: 100%; height: 100%;"
+      :center="center"
+      :zoom="11"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+  </div>
 </template>
